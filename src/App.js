9220638a3796565eeb75e1657f72b95cdb3e1415
Wei He <git@weispot.com>
2020-11-08 16:33:09 -0500

    Reproduce aws-amplify/amplify-js#6693

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Storage } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+import { useState } from 'react';
 
 function App() {
+  const [testText, setTestText] = useState('')
+
+  async function doStuff() {
+    const putResult = await Storage.put('test.json', `{"text":"Hello ${new Date().toISOString()}"}`, { level: 'private', contentType: 'application/json' })
+    console.log(putResult)
+
+    const data = await Storage.get(`test.json`, { level: 'private', download: true })
+    const text = (await new Response(data.Body).json()).text;
+    console.log(text)
+    setTestText(text)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{testText}</h1>
+        <button onClick={doStuff}>Do Stuff</button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
